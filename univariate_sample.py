34b1c4042705aceaa3bdbbbde0b35f245ac964d7
Sobal Ivan <57543655+Anonimprogramer@users.noreply.github.com>
2021-11-14 20:55:31 +0300

    add new grapic

--- univariate_sample.py
+++ univariate_sample.py
@@ -44,7 +44,7 @@ def distribution_function_grapic(variation_range):
     plt.show()
 
 
-def equiprobable_method_grapic(variation_range):
+def equiinterval_method_grapic(variation_range):
     n = int(sqrt(len(variation_range)))
     delta = (variation_range[len(variation_range) - 1] - variation_range[0]) / n
     delta_lists = []
@@ -58,16 +58,40 @@ def equiprobable_method_grapic(variation_range):
     y = [0]
     i = 0
     for list in delta_lists:
-        x.append(delta*i)
-        x.append(delta*(i+1))
-        y.append(len(list)/n/delta)
-        y.append(len(list)/n/delta)
-        i+=1
+        x.append(delta * i)
+        x.append(delta * (i + 1))
+        y.append(len(list) / n / delta)
+        y.append(len(list) / n / delta)
+        i += 1
     y.append(0)
-    x.append(delta*n)
+    x.append(delta * n)
 
-
-    plt.plot(x,y)
+    plt.plot(x, y)
     plt.show()
-    print(x)
-    print(y)
+
+
+def equipropable_method_grapic(variation_range):
+    n = int(sqrt(len(variation_range)))
+    list_a = [variation_range[0]]
+    list_b = []
+    for i in range(1, 10):
+        list_a.append((variation_range[i * n] + variation_range[i * n + 1]) / 2)
+        list_b.append((variation_range[i * n] + variation_range[i * n + 1]) / 2)
+    list_b.append(variation_range[len(variation_range)-1])
+
+    x = [0]
+    y = [0]
+
+    for i in range(n):
+        x.append(list_a[i])
+        x.append((list_b[i]))
+        y.append(1/(n*list_b[i]-list_a[i]))
+        y.append(1 / (n * list_b[i] - list_a[i]))
+
+    x.append(variation_range[len(variation_range)-1])
+    y.append(0)
+
+    plt.plot(x, y)
+    plt.show()
+
+
