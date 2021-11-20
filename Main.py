bcd33869a7e27226135b15350be66fab3c62a442
Sobal Ivan <57543655+Anonimprogramer@users.noreply.github.com>
2021-11-21 02:10:03 +0300

    добавление двумерной выборки

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 import univariate_sample as us
 
+import bivariate_sample as bs
+
 univariate_sample = us.load("univariate_input.txt")
 
 variety_range = us.variation_range_of_univariate_sample(univariate_sample)
@@ -23,3 +25,15 @@ us.distribution_function_grapic_and_even_distributin_function(variety_range)
 us.distribution_function_grapic_and_other_function(variety_range)
 
 us.hypotize_of_normal_law(variety_range)
+
+print("А вот тут как бы одиночная выборка закончилась, начинается двумерная,типа пары какие-то,хз матеша это какой-то "
+      "кибербуллинг одиноких людей, я имею ввиду что дальше?Семейство первообразных?(если что,то я просто рофлю)")
+
+x = []
+y = []
+bs.load(x, y, "bivariate_input.txt")
+print(bs.correlation_moment(x,y),"- оценка корреляционного момента")
+print(bs.correlation_coefficent(x,y),"-точечная оценка коэффициент корреляции")
+print(bs.interval_correlation_mark(x,y),"- интервальная оценка коэффициента корреляции с надежностью γ = 0,95")
+bs.hypotize_of_lack_correlation(x,y)
+bs.dispersion_diargram_and_regression_line(x,y)
