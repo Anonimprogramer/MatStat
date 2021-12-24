f2386005a045cac909ead928ffd5eed09312b500
kilativ-dotcom <56753138+kilativ-dotcom@users.noreply.github.com>
2021-12-24 17:04:37 +0300

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,11 +1,11 @@
 import univariate_sample as us
 import bivariate_sample as bs
 
-print("-----Одномерная выборка------")
+print("-----univariate_input------")
 univariate_sample = us.load("univariate_input.txt")
 
 variety_range = us.variation_range_of_univariate_sample(univariate_sample)
-print("Вариационный ряд величины X")
+print("Variation row X")
 print(variety_range)
 
 us.distribution_function_grapic(variety_range)
@@ -14,9 +14,9 @@ us.equiinterval_method_grapic(variety_range)
 
 us.equipropable_method_grapic(variety_range)
 
-print(us.expectation(variety_range), "- точечное мат ожидание")
+print(us.expectation(variety_range), "- m")
 
-print(us.dispersion(variety_range), "- точечная дисперсия")
+print(us.dispersion(variety_range), "- D")
 
 us.confidence_interval_MAT(variety_range)
 
@@ -26,7 +26,7 @@ us.distribution_function_grapic_and_other_function(variety_range)
 
 us.hypotize_of_normal_law(variety_range)
 
-print("-----Двумерная выборка------")
+print("-----bivariate_input------")
 x = []
 y = []
 bs.load(x, y, "bivariate_input.txt")
@@ -34,9 +34,9 @@ print("Mx-", us.expectation(x))
 print("My-", us.expectation(y))
 print("Dx-", us.dispersion(x))
 print("Dy-", us.dispersion(y))
-print(bs.correlation_moment(x,y),"- оценка корреляционного момента")
-print(bs.correlation_coefficent(x,y),"-точечная оценка коэффициент корреляции")
-print(bs.interval_correlation_mark(x,y),"- интервальная оценка коэффициента корреляции с надежностью γ = 0,95")
+print(bs.correlation_moment(x,y),"- correlation moment")
+print(bs.correlation_coefficent(x,y),"-correlation coefficient")
+print(bs.interval_correlation_mark(x,y)," -correlation coefficient with gamma = 0,95")
 bs.hypotize_of_lack_correlation(x,y)
 bs.dispersion_diargram_and_regression_line(x,y)
 
